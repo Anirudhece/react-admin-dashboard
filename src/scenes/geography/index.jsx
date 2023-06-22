34fcd365452f8b85ff7f8204ba491c6c1f9af56c
Anirudh <anirudhece2020@gmail.com>
2023-06-22 15:47:36 +0530

    geography updated

--- src/scenes/geography/index.jsx
+++ src/scenes/geography/index.jsx
@@ -8,7 +8,7 @@ const Geography = () => {
   const colors = tokens(theme.palette.mode);
   return (
     <Box m="20px">
-      <Header title="Geography" subtitle="Simple Geography Chart" />
+      <Header title="Geography" subtitle="Here I wish to display only the Indian map, but due to some issues, I have not been able to do so yet." />
 
       <Box
         height="75vh"
